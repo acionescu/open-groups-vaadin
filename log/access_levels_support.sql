68c3749595477efa60c757893a6af39d54e7498a
acionescu <adrian.ionescu.consulting@gmail.com>
2012-08-09 21:18:15 +0300

    changes

--- log/access_levels_support.sql
+++ log/access_levels_support.sql
@@ -1,3 +1,21 @@
+-- Table: access_permissions
+
+-- DROP TABLE access_permissions;
+
+CREATE TABLE access_permissions
+(
+  id bigint NOT NULL,
+  name character varying(50) NOT NULL,
+  CONSTRAINT access_permissions_pk PRIMARY KEY (id )
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE access_permissions
+  OWNER TO metaguvernare;
+
+
+
 -- Sequence: access_rules_seq
 
 -- DROP SEQUENCE access_rules_seq;
@@ -10,7 +28,10 @@ CREATE SEQUENCE access_rules_seq
   CACHE 1;
 ALTER TABLE access_rules_seq
   OWNER TO metaguvernare;
-
+  
+  
+  
+  
 -- Table: access_rules
 
 -- DROP TABLE access_rules;
@@ -18,27 +39,61 @@ ALTER TABLE access_rules_seq
 CREATE TABLE access_rules
 (
   id bigint NOT NULL DEFAULT nextval('access_rules_seq'::regclass),
-  access_level bigint NOT NULL,
   name character varying(50) NOT NULL,
+  group_permissions_id bigint,
+  others_permissions_id bigint,
   active character(1) NOT NULL DEFAULT 'y'::bpchar,
-  CONSTRAINT access_rules_pk PRIMARY KEY (id )
+  CONSTRAINT access_rules_pk PRIMARY KEY (id ),
+  CONSTRAINT access_rules_groups_permissions_fk FOREIGN KEY (group_permissions_id)
+      REFERENCES access_permissions (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT access_rules_others_permissions_fk FOREIGN KEY (others_permissions_id)
+      REFERENCES access_permissions (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
   OIDS=FALSE
 );
 ALTER TABLE access_rules
   OWNER TO metaguvernare;
-  
+ 
+-- Table: action_types_permissions
 
-  
-insert into access_rules(access_level,name) values(0,'PRIVATE');
-insert into access_rules(access_level,name,active) values(1,'PRIVATELY_SHARED','n');
-insert into access_rules(access_level,name,active) values(2,'PUBLICLY_SHARED','n');  
-  
+-- DROP TABLE action_types_permissions;
+
+CREATE TABLE action_types_permissions
+(
+  permission_id bigint NOT NULL,
+  action_type_id bigint NOT NULL,
+  CONSTRAINT action_types_permissions_pk PRIMARY KEY (permission_id , action_type_id ),
+  CONSTRAINT action_types_permissions_action_types_fk FOREIGN KEY (action_type_id)
+      REFERENCES action_types (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT action_types_permissions_permissions_fk FOREIGN KEY (permission_id)
+      REFERENCES access_permissions (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE action_types_permissions
+  OWNER TO metaguvernare;
+
 
 alter table entities add column access_rule_id bigint null;
 alter table entities add constraint entities_access_rules_fk foreign key (access_rule_id) references access_rules (id);
 
 alter table entities add column group_id bigint null;
 alter table entities add constraint entity_group_fk foreign key (group_id) references entities (id);
+
+
+insert into access_permissions values(1,'READONLY');
+
+insert into action_types_permissions values((select id from access_permissions where name='READONLY'),(select id from action_types where type='READ'));
+
+insert into access_rules(name,group_permissions_id,others_permissions_id) values('PRIVATE',null,null);
+
+insert into access_rules(name,group_permissions_id,others_permissions_id) values('READONLY',(select id from access_permissions where name='READONLY'),(select id from access_permissions where name='READONLY'));
+
+
   
