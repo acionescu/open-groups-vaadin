fd705c65598b623018efc16952d8b6e6460a982e
acionescu <adrian.ionescu.consulting@gmail.com>
2012-08-23 15:18:22 +0300

    moved logs and specs to netcell-node

--- log/config.sql
+++
@@ -1,4 +0,0 @@
-insert into application_config(param_name,string_value) values('is.instance.private','y');
-insert into application_config(param_name,string_value) values('allowed.openid.domains','qualitance.ro, qualitance.com');
-insert into application_config(param_name,string_value) values('instance.name','Qualitance knowledge sharing');
-insert into application_config(param_name,string_value) values('instance.url','http://ks.qualitance.ro/')
